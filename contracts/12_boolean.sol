41f831829cfd49335c7bacc7bf895e8d9de8acbf
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-05 18:11:13 +0530

    boolean

---
+++ contracts/12_boolean.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract boolean {
+    // Default value of bool is false
+    bool public defaultBool;
+    // We can init the value as either true or false only
+    bool public check = true;
+}
