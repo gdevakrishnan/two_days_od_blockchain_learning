7218c8013a7f2ba474923e17b66bcba714d6a33b
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-05 12:34:53 +0530

    comments in solidity

---
+++ contracts/6_comments.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract comments {
+    string public str = "Comments in Solidity";
+    // Single Line Comment syntax
+
+    /*
+        multiline
+        comment
+        syntax
+    */
+}
