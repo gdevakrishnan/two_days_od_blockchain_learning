28029a05e70beea0c0fcb0c8080013090f6d1b91
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-10-27 12:10:11 +0530

    fixed type array

--- contracts/16_fixed_arrays.sol
+++ contracts/16_fixed_arrays.sol
@@ -2,5 +2,13 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 contract fixed_array {
-    
+    uint[4] myArr = [10, 20, 30, 40];
+
+    function getter() public view returns (uint[4] memory) {
+        return myArr;
+    }
+
+    function setter(uint _index, uint _value) public {
+        myArr[_index] = _value;
+    }
 }
