35cd66a5712a67d6959c106c65227984a73e91c9
gdevakrishnan <gdevakrishnan700@gmail.com>
2023-11-30 20:57:32 +0530

    Crowd Funcding 2

--- contracts/projects/6_crowd_funding.sol
+++ contracts/projects/6_crowd_funding.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CrowdFunding {
+contract crowdFunding {
     address NGO;
-    address payable[] donaters;
-
+    
     constructor () {
         NGO = msg.sender;
     }
 
-    function getToken() public payable {
-        (msg.value >= 1 ether, "You needs to pay minimum 1 ether to donate");
+    function giveFund () payable public {
+        require(msg.value >= 1 ether, "You needs to give fund minimum 1 ether");
     }
 
-    function getBalance() internal view returns (uint){
+    function getBalance() internal view returns (uint) {
         return address(this).balance;
     }
 
-    function donate(address payable _orphanage, uint _minEther) payable public {
-        require(NGO == msg.sender, "You are not the NGO");
-        uint balance = getBalance();
-        require(balance > _minEther, "There is minimum ether");
-        _orphanage.transfer(_minEther);
-        donaters = new address payable[](0);
-    } 
+    function transferToOrphanage(address payable _orphanageAddress, uint _minEth) payable public {
+        require(NGO == msg.sender, "You are not from that NGO");
+        uint balanceEth = getBalance();
+        require(balanceEth >= _minEth, "There is minimum balance");
+        _orphanageAddress.transfer(balanceEth);
+    }
 }
